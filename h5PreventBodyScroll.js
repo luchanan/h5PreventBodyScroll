a73aaf1f8907a735aebe1877f0ad0344796d38ff
“yluchanan <398778994@qq.com>
2016-09-23 19:13:47 +0800

    test

--- h5PreventBodyScroll.js
+++ h5PreventBodyScroll.js
@@ -11,27 +11,27 @@
         mask:true,
         maskClick:true,
     };
-    var methods=function(){
-        return{
-            disableBodyScroll:disableBodyScroll
-        }
-    }
+    var disableBodyScroll = function(){
+        console.log("disableBodyScroll");
+        document.addEventListener('touchmove',preventDefault);
+    };
     var preventDefault=function(e) {
         e.preventDefault();
     }
     var stopPropagation=function(e){
         e.stopPropagation();
     }
-    var disableBodyScroll = function(){
-        console.log("disableBodyScroll");
-        document.addEventListener('touchmove',preventDefault);
-    };
+
     var enableBodyScroll = function(){
         document.removeEventListener('touchmove',preventDefault);
     };
     var elementsScroll=function(){
         console.log("a");
     }
+    /*var methods={
+        disableBodyScroll:disableBodyScroll,
+        test:test
+    }*/
     function preventBodyScroll(options){
         this.settings=$.extend({},defaults,options || {});
     }
@@ -39,12 +39,12 @@
         init:function(options){
             console.log("a");
         },
-        methods:methods,
+        //methods:methods,
         disableBodyScroll:disableBodyScroll
     }
-    var h5PreventBodyScroll=function(options){
-        return new preventBodyScroll(options);
-    }
-    //window.h5PreventBodyScroll=preventBodyScroll;
-    window.h5PreventBodyScroll=h5PreventBodyScroll;
+    // var h5PreventBodyScroll=function(options){
+    //     return new preventBodyScroll(options);
+    // }
+    //window.h5PreventBodyScroll=h5PreventBodyScroll;
+    window.h5PreventBodyScroll=preventBodyScroll;
 }));
