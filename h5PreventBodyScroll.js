be38914c5ad810bf9b69dd244d39ab4d3f89a0b8
“luchanan <398778994@qq.com>
2016-09-25 23:25:08 +0800

    update

--- h5PreventBodyScroll.js
+++ h5PreventBodyScroll.js
@@ -7,13 +7,24 @@
         factory(jQuery);
     }
 }(function ($) {
+    var startX,startY;
     var defaults ={
         mask:true,
         maskClick:true,
+        appendEle:'body',
+        maskClickName:".h5PreventBodyScroll",
+        onEventBindTo:document,
+        triggerEle:null,
+        scrollEle:[],
+        scrollTemp:'',
     };
     var disableBodyScroll = function(){
-        console.log("disableBodyScroll");
         document.addEventListener('touchmove',preventDefault);
+        document.addEventListener('mousewheel',preventDefault);
+    };
+    var enableBodyScroll = function(){
+        document.removeEventListener('touchmove',preventDefault);
+        document.removeEventListener('mousewheel',preventDefault);
     };
     var preventDefault=function(e) {
         e.preventDefault();
@@ -21,12 +32,17 @@
     var stopPropagation=function(e){
         e.stopPropagation();
     }
-
-    var enableBodyScroll = function(){
-        document.removeEventListener('touchmove',preventDefault);
-    };
-    var elementsScroll=function(){
-        console.log("a");
+    var elementsScroll=function(e){
+        for(var i=0;i<this.settings.scrollEle.length;i++){
+            $(this.settings.onEventBindTo).on("touchstart",this.settings.scrollEle[i],function(e){
+                startX = e.originalEvent.changedTouches[0].pageX;/*jquery不能使用e.changedTouches[0].pageX获取坐标*/
+                startY = e.originalEvent.changedTouches[0].pageY;
+            });
+            $(this.settings.onEventBindTo).on("touchmove",this.settings.scrollEle[i],function(e){
+                 var rangeX=e.originalEvent.changedTouches[0].pageX-startX;
+                 var rangeY=e.originalEvent.changedTouches[0].pageY-startY;
+            });
+        }
     }
     /*var methods={
         disableBodyScroll:disableBodyScroll,
@@ -36,11 +52,48 @@
         this.settings=$.extend({},defaults,options || {});
     }
     preventBodyScroll.prototype={
-        init:function(options){
-            console.log("a");
+        init:function(){
+            if($(this.settings.triggerEle).attr("show")!=undefined){
+                this.show("#"+$(this.settings.triggerEle).attr("show"));
+            }
+            else{
+                this.htmlTemplate();
+            }
+            this.disableBodyScroll();
         },
-        //methods:methods,
-        disableBodyScroll:disableBodyScroll
+        htmlTemplate:function(){
+            var randomId="id"+(new Date()).getTime();
+            this.settings.html=$('<div class="h5PreventBodyScroll '+(this.settings.mask?'':'maskFalse')+'" id="'+randomId+'"></div>');
+            this.settings.html.appendTo(this.settings.appendEle);
+            $(this.settings.triggerEle).attr("show",randomId);
+            this.bindEvent();
+        },
+        bindEvent:function(){
+            if(this.settings.maskClick){
+                /*被点击绑定在document上在IOS无效（测试系统9.3），解决方法：在被点击事件加上cursor: pointer;样式*/
+                $(this.settings.onEventBindTo).on("click",this.settings.maskClickName,$.proxy(function(event){
+                     var $target = $(event.target);
+                     if($target.is(this.settings.maskClickName)){
+                        this.closed(this.settings.html.attr("id"));
+                     }
+                },this));
+            }
+        },
+        show:function(id){
+            $(id).show();
+        },
+        closed:function(id){
+            if(id==undefined){
+                $(this.settings.maskClickName).hide();
+            }
+            else{
+                $("#"+id).hide();
+            }
+            this.enableBodyScroll();
+        },
+        disableBodyScroll:disableBodyScroll,
+        enableBodyScroll:enableBodyScroll,
+        elementsScroll:elementsScroll
     }
     // var h5PreventBodyScroll=function(options){
     //     return new preventBodyScroll(options);
